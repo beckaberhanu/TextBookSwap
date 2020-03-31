a1aaaf32a7cf9182f23da36ce137123280797722
nadavskloot <nskloot@macalester.edu>
2020-03-31 15:44:50 -0700

    Switched from sqlite to postgres

--- textbookswap/textbookswap/settings.py
+++ textbookswap/textbookswap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import getpass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'textbookswap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': getpass.getuser(),
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- textbookswap/users/models.py
+++ textbookswap/users/models.py
@@ -27,34 +27,34 @@ class WishList(models.Model):
 
 
 # I realize that this isn't part of our MVP but I'm putting it here in case want to implement it
-class MessageChain(models.Model):
-    # many-1 field pointing to correspoding post
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    # many-1 field pointing to the seller. Could just reference the 'User' from the 'Post' model but this seemed clearer.
-    seller = models.ForeignKey(User, on_delete=models.CASCADE)
-    # many-1 field pointing to correspoding post
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
-    # still thinking about how the buyer would reference their message chain
+# class MessageChain(models.Model):
+#     # many-1 field pointing to correspoding post
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     # many-1 field pointing to the seller. Could just reference the 'User' from the 'Post' model but this seemed clearer.
+#     seller = models.ForeignKey(User, on_delete=models.CASCADE)
+#     # many-1 field pointing to correspoding post
+#     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+#     # still thinking about how the buyer would reference their message chain
 
 
-class Messages(models.Model):
-    chain = models.ForeignKey(MessageChain, on_delete=models.CASCADE)
-    content = models.TextField(max_length=1000)
-    time_sent = models.DateTimeField(default=timezone)
-    # ***************************************************************
-    SELLER = 'Seller'
-    BUYER = 'Buyer'
-    choices = (
-        (SELLER, 'Seller'),
-        (BUYER, 'Buyer')
-    )
-    sender = models.CharField(max_length=10, choices=choices)
-    # ***************************************************************
-    TEXT = 'Text'
-    OFFER = 'Offer'
-    message_types = (
-        (TEXT, 'Text'),
-        (OFFER, 'Offer')
-    )
-    message_type = models.CharField(max_length=10, choices=message_types)
-    # ***************************************************************
+# class Messages(models.Model):
+#     chain = models.ForeignKey(MessageChain, on_delete=models.CASCADE)
+#     content = models.TextField(max_length=1000)
+#     time_sent = models.DateTimeField(default=timezone)
+#     # ***************************************************************
+#     SELLER = 'Seller'
+#     BUYER = 'Buyer'
+#     choices = (
+#         (SELLER, 'Seller'),
+#         (BUYER, 'Buyer')
+#     )
+#     sender = models.CharField(max_length=10, choices=choices)
+#     # ***************************************************************
+#     TEXT = 'Text'
+#     OFFER = 'Offer'
+#     message_types = (
+#         (TEXT, 'Text'),
+#         (OFFER, 'Offer')
+#     )
+#     message_type = models.CharField(max_length=10, choices=message_types)
+#     # ***************************************************************
