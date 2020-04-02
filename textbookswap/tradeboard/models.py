ca51447f920ba3bdd304bde2a3f02de9d1c95f5f
beckaberhanu <beckaberhanu2@gmail.com>
2020-04-02 17:26:07 -0500

    fixed problem with MessageChain and Message models and added Edition field for the post model

--- textbookswap/tradeboard/models.py
+++ textbookswap/tradeboard/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.core.exceptions import ValidationError
 
 
 class Post(models.Model):
     seller = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=100)
-    ISBN = models.CharField(max_length=13)
+
+    def validate_ISBN(self, value):
+        if (len(value) != 10 or len(value) != 13):
+            raise ValidationError(f'number of digits in {value} is neither 10 nor 13',
+                                  params={'value': value},
+                                  )
+        elif (not(f'{value}'.isnumeric())):
+            raise ValidationError(f'{value} has non-numeric elements',
+                                  params={'value': value},
+                                  )
+
+    ISBN = models.CharField(max_length=13, validators=[validate_ISBN])
 
     author = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
@@ -16,11 +26,18 @@ class Post(models.Model):
         default='default_book.png', upload_to='book_pics')
     date_posted = models.DateTimeField(default=timezone.now)
 
-    IN_PROGRESS = "in progress"
-    COMPLETE = "complete"
+    edition = models.PositiveSmallIntegerField()
+
+    IN_PROGRESS = "In progress"
+    COMPLETE = "Complete"
     states = (
-        (IN_PROGRESS, "in progress"),
-        (COMPLETE, "complete"),
+        (IN_PROGRESS, "In progress"),
+        (COMPLETE, "Complete"),
     )
     transaction_state = models.CharField(
         max_length=50, choices=states, default=IN_PROGRESS)
+
+    class Meta:
+        verbose_name = 'Post'
+        # Name the model will appear under in the Django Admin page.
+        verbose_name_plural = 'Posts'
--- textbookswap/users/models.py
+++ textbookswap/users/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from tradeboard.models import Post
 
-# Create your models here.
-
 
 class Profile(models.Model):
     # 1-1 field pointing to correspoding user
@@ -14,6 +12,11 @@ class Profile(models.Model):
 
     def __str__(self):  # similar to a 'tostring' method in java
         return f"{self.user.username} Profile"
+
+    class Meta:
+        verbose_name = 'Profile'
+        # Name the model will appear under in the Django Admin page.
+        verbose_name_plural = 'Profiles'
 
 
 # may need to reconsider the name. For the functionality it is serving right now it maybe batter to just call it bookmarks.
@@ -25,36 +28,57 @@ class WishList(models.Model):
     def __str__(self):
         return f"{self.user.username}'s wishlist: \n {self.posts.all()}"
 
+    class Meta:
+        verbose_name = 'Wishlist'
+        # Name the model will appear under in the Django Admin page.
+        verbose_name_plural = 'Wishlists'
+
 
 # I realize that this isn't part of our MVP but I'm putting it here in case want to implement it
-# class MessageChain(models.Model):
-#     # many-1 field pointing to correspoding post
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-#     # many-1 field pointing to the seller. Could just reference the 'User' from the 'Post' model but this seemed clearer.
-#     seller = models.ForeignKey(User, on_delete=models.CASCADE)
-#     # many-1 field pointing to correspoding post
-#     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
-#     # still thinking about how the buyer would reference their message chain
+class MessageChain(models.Model):
+    # many-1 field pointing to correspoding post
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # many-1 field pointing to the seller. Could just reference the 'User' from the 'Post' model but this seemed clearer.
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="messagechain_selling")
+    # many-1 field pointing to correspoding post
+    buyer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="messagechain_buying")
+    # still thinking about how the buyer would reference their message chain
+
+    def __str__(self):
+        return f'Massage Chain[Post ID: {post.pk}, Seller: {seller.username}, Buyer: {buyer.username}]'
+
+    class Meta:
+        verbose_name = 'Message-Chain'
+        # Name the model will appear under in the Django Admin page.
+        verbose_name_plural = 'Message-Chains'
 
 
-# class Messages(models.Model):
-#     chain = models.ForeignKey(MessageChain, on_delete=models.CASCADE)
-#     content = models.TextField(max_length=1000)
-#     time_sent = models.DateTimeField(default=timezone)
-#     # ***************************************************************
-#     SELLER = 'Seller'
-#     BUYER = 'Buyer'
-#     choices = (
-#         (SELLER, 'Seller'),
-#         (BUYER, 'Buyer')
-#     )
-#     sender = models.CharField(max_length=10, choices=choices)
-#     # ***************************************************************
-#     TEXT = 'Text'
-#     OFFER = 'Offer'
-#     message_types = (
-#         (TEXT, 'Text'),
-#         (OFFER, 'Offer')
-#     )
-#     message_type = models.CharField(max_length=10, choices=message_types)
-#     # ***************************************************************
+class Message(models.Model):
+    chain = models.ForeignKey(
+        MessageChain, on_delete=models.CASCADE,  related_name="messages")
+    content = models.TextField(max_length=1000)
+    time_sent = models.DateTimeField(default=timezone.now)
+    # ***************************************************************
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="messages_sent")
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="messages_recieved")
+    # ***************************************************************
+    TEXT = 'Text'
+    OFFER = 'Offer'
+    message_types = (
+        (TEXT, 'Text'),
+        (OFFER, 'Offer')
+    )
+    message_type = models.CharField(max_length=10, choices=message_types)
+    # ***************************************************************
+
+    def __str__(self):
+        return f'Message [Post: {chain.post.pk}, From: {sender.username}, Buyer: {receiver.username}]'
+
+    class Meta:
+        verbose_name = 'Message'
+        # Name the model will appear under in the Django Admin page.
+        verbose_name_plural = 'Messages'
