dfede9e469826b488cda097edce7c7e65a273338
nadavskloot <nskloot@macalester.edu>
2020-04-04 16:46:41 -0700

    Added functionality for posting a book for sale.

--- textbookswap/textbookswap/urls.py
+++ textbookswap/textbookswap/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from tradeboard import views as tradeboard_view
+from tradeboard.views import BookCreateView
 
 urlpatterns = [
+    path('book/new/', BookCreateView.as_view(), name='book_new'),
     path('admin/', admin.site.urls),
     path('', tradeboard_view.home, name='tradeboard-home')
 ]
--- textbookswap/tradeboard/views.py
+++ textbookswap/tradeboard/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from .models import Post
 from .instances import createInstances
 from .instances import deleteAllInstances
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def home(request):
     createInstances()
     return render(request, 'tradeboard/home.html')
+
+class BookCreateView(CreateView):
+    model = Post
+    template_name = "tradeboard/new_book.html"
+    fields = '__all__'
