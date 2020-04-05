d2da31ebd22c5075883a3f536bfcb94917d3623b
beckaberhanu <beckaberhanu2@gmail.com>
2020-04-05 18:50:30 -0500

    improvements to createInstances.py so it generates lorem ipsum text for post descriptions, home page shows all posts, home view now passes in all posts as a context

--- textbookswap/tradeboard/views.py
+++ textbookswap/tradeboard/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import Post
-from .instances import createInstances, deleteAllInstances
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
 # Create your views here.
+
+
 def home(request):
-    createInstances()
-    return render(request, 'tradeboard/home.html')
+    posts = Post.objects.all()
+    return render(request, 'tradeboard/home.html', {'posts': posts})
+
 
 class BookCreateView(CreateView):
     model = Post
