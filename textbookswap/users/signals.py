f1d544abc88c55fe310b9f4b3f8ff9844ce19291
TianruiLiu <tliu1@macalester.edu>
2020-04-12 22:02:06 -0500

    Fixed profile issue and added navigations to the home page

--- textbookswap/users/signals.py
+++ textbookswap/users/signals.py
@@ -4,9 +4,11 @@ from django.dispatch import receiver
 from .models import Profile
 
 
-@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
-def save_profile(sender, instance, created, **kwargs):
-    user = instance
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
     if created:
-        profile = Profile(user=user)
-        profile.save()
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
