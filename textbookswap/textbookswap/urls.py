5c7825f07f280303f4f72187c8a21ac2102978b3
Qingru Zhang <qzhang1@macalester.edu>
2020-04-14 11:43:33 +0800

    A small change to urls so that you need to login before coming to home page.

--- textbookswap/textbookswap/urls.py
+++ textbookswap/textbookswap/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', tradeboard_view.home, name='tradeboard-home')
+    path('',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('home/', tradeboard_view.home, name='tradeboard-home')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
