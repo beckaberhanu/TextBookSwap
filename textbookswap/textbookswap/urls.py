98ac9419a83468b828fd3312c99923f146101130
TianruiLiu <tliu1@macalester.edu>
2020-04-23 13:47:03 -0500

    solved a merge conflict

--- textbookswap/textbookswap/urls.py
+++ textbookswap/textbookswap/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-<<<<<<< HEAD
     path('password-reset/', 
         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', 
@@ -27,10 +26,6 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>', 
         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-=======
-    path('', auth_views.LoginView.as_view(
-        template_name='users/login.html'), name='login'),
->>>>>>> 58011cdefa43b214d975b36356d50f23f8afcc41
     path('home/', tradeboard_view.home, name='tradeboard-home')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
